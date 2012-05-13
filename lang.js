508aca16ac76edf688625758a19c17d5c9608ad4
Mike Wilcox <mike.wilcox@bettervideo.com>
2012-05-13 16:07:28 -0500

    Doc'd lang

--- lang.js
+++ lang.js
@@ -1,7 +1,15 @@
 define([
 	'dojo/_base/lang'
 ], function(lang){
-
+	//	summary:
+	//		The export of this module contains some common Dojo lang methods,
+	//		generally using shorter names. It also contains some unique methods.
+	//	returns: Object
+	//
+	//	description:
+	//		The methods provided and their maps to the Dojo equivalents are:
+	//			lang.bind: lang.hitch
+	//			lang.mix: lang.mixin
 	var
 		_uidMap = {},
 		uid = function(str){
@@ -14,16 +22,41 @@ define([
 
 	return {
 
-		uid:uid,
-		bind:lang.hitch,
-		hitch:lang.hitch,
-		mix:lang.mixin,
+		uid:
+			//	summary:
+			//		Returns a unique ID. Accepts and optional string for a
+			//		prefix.
+			uid,
 
-		last: function(array){
+		bind:
+			//	summary:
+			//		Renames dojo.hitch.
+			lang.hitch,
+
+		hitch:
+			//	summary:
+			//		Shortcut to dojo.hitch.
+			lang.hitch,
+
+		mix:
+			//	summary:
+			//		Shortened name for dojo.mixin.
+			lang.mixin,
+
+		last: function(/*Array*/array){
+			// 	summary:
+			// 		Returns the last element of an array.
 			return array[array.length -1];
 		},
 
-		minMax: function(num, n1, n2){
+		minMax: function(/*Number*/num, /*Number*/n1, /*Number*/n2){
+			//	summary:
+			//		Returns the number if it is inbetween the min and the max.
+			//		If it is over it returns the max, if under returns the min.
+			//	Returns Number
+			//
+			//	TODO: May rename to clamp()
+			//
 			var min, max;
 			if(n1 > n2){
 				min = n2; max = n1;
