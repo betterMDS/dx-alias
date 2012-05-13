a3a3fd09e9e2e430125abd8d3292b5e1e2e651fe
Mike Wilcox <mike.wilcox@bettervideo.com>
2012-05-13 16:54:16 -0500

    doc'd string

--- string.js
+++ string.js
@@ -1,8 +1,27 @@
 define([], function(){
-
+	//	summary:
+	//		Not an alias to any Dojo modules (except trim). This contains all
+	//		unique functionality.
+	//
 	return {
 
-		urlToObj: function(url){
+		urlToObj: function(/*String?*/url){
+			//	summary:
+			//		Converts an URL into an object.
+			//	url:String?
+			//		Some kind of URL. If undefined, uses current document URL.
+			//	returns: Object
+			//		protocol: http or https, etc
+			//		host: the entire domain (and sub-domain if available)
+			//		href: The entire URL
+			//		hash: Any text after the # sign (if available)
+			//		query: Any text after the ? sign (if available)
+			//		filename: Name of the file pointed to (handy for images)
+			//		ext: Extension of the file pointed to (handy for images)
+			//		folder: path, minus filename and domain
+			//		sameDomain: Boolean, if a passed URL is not in the same
+			//			domain as the current document
+			//
 			var loc = location, q;
 			url = url || loc.href;
 			if(/\?|\#/.test(url)){
@@ -34,7 +53,13 @@ define([], function(){
 			return o;
 		},
 
-		strToObj: function(str){
+		strToObj: function(/*String*/str){
+			//	summary:
+			//		Converts a string to an object. Similar to Dojo's fromQuery,
+			//		but more, uh... specific to my needs :)
+			//		Notably, it will normalize values (into numbers or booleans)
+			//		and trim file names.
+			//
 			if(!str) return {};
 			var delim = /&/.test(str) ? "&" : ",",
 				eq = /=/.test(str) ? "=" : ":",
@@ -52,7 +77,9 @@ define([], function(){
 			return o;
 		},
 
-		trim: function(str){
+		trim: function(/*String*/str){
+			// summary:
+			// 		Everyone knows what this is.
 			return !str ?
 				str :
 				str.trim ?
@@ -60,8 +87,12 @@ define([], function(){
 					str.replace(/^\s\s*/, '').replace(/\s\s*$/, '');
 		},
 
-		normalize: function(v, delim){
-			//console.trace(); console.log("value:", v)
+		normalize: function(/*Something*/v, /*String?*/delim){
+			//	summary:
+			//		Normalizes a value. Converts strings into numbers or
+			//		booleans if applicable. Tests for an optional delimeter,
+			//		and also commas, which will convert into an Array.
+			//
 			if(v == "true") return true;
 			if(v == "false") return false;
 			if(v.substring(0,1) == "0") return v; // 001 id
@@ -75,8 +106,9 @@ define([], function(){
 			return v;
 		},
 
-		urlEscape: function(s){
-			// blocks unicode chars
+		urlEscape: function(/*String*/s){
+			//	summary:
+			// 		Blocks unicode chars
 			var t = "";
 			for(var i=0;i<s.length;i++){
 				if(s.charCodeAt(i) < 255) t+=s.charAt(i);
@@ -84,7 +116,10 @@ define([], function(){
 			return escape(t);
 		},
 
-		urlEncode: function(str){
+		urlEncode: function(/*String*/str){
+			// summary:
+			// 		Shortcut for encodeURI, because I never remember the name
+			// 		of it. :(
 			return encodeURI(this.trim(str).replace(" ", "+"));
 			//encodeURIComponent
 		}
