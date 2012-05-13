aef14fae9b56e9e65d0efc08564d287a3d1b02fc
Mike Wilcox <mike.wilcox@bettervideo.com>
2012-05-13 17:10:28 -0500

    Doc'd fx

--- fx.js
+++ fx.js
@@ -4,12 +4,35 @@ define([
 	'./on',
 	'./log'
 ], function(has, dom, on, logger){
-
+	//	summary:
+	//		A module designed to contain unique effects. the only useful method
+	//		is "flyout"; otherwise this is under development.
+	//
 	var log = logger('FX', 1);
 	var fx = {
 
-		flyout: function(btnNode, displayNode, options){
-
+		flyout: function(/*DOMNode*/btnNode, /*DOMNode*/displayNode, /*Object*/options){
+			//	summary:
+			//		A helper method to control a flyout node via a button or
+			//		other node. Think of a volume slider popping up when you
+			//		mouse over the volume button.
+			//	returns: undefined
+			//
+			//	btnNode:DOMNode
+			//		The button that controls the flyout node.
+			//	displayNode:DOMNode
+			//		The node that appears when the button activates it.
+			//	options:Object (all optional)
+			//		onHide:Function
+			//			The function to call when the mouseout or click-off
+			//			resolves. If not passed, a native hide() is used.
+			//		onShow:Function
+			//			The function to call when the mouseover or click
+			//			resolves. If not passed, a native show() is used.
+			//		onover:Boolean?
+			//			If true, the flyout shows on mouseover. If falsey, the
+			//			flyout shows on click, and hides on click-off.
+			//
 			var onover = options.onover;
 
 			var displayNodeIsOver = false, btnIsOver = false, tmr, showing = false;
@@ -100,7 +123,7 @@ define([
 		transistion: function(node, type, options){
 			//
 			// under construction. Not ready for use.
-			// 
+			//
 			options = options || {};
 			var show, hide;
 			var transition = has('transition');
