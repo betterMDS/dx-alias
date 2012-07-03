5de91c688349350bbd8352cf93dfd9261abf92f1
Mike Wilcox <mike.wilcox@bettervideo.com>
2012-07-03 13:25:44 -0500

    Docs

--- Widget.js
+++ Widget.js
@@ -6,7 +6,11 @@ define([
 	'dx-alias/dom',
 	'dx-alias/on'
 ],function(declare, registry, _WidgetBase, _TemplatedMixin, dom, on){
-
+	//	summary:
+	//		Widget is not very complicated, it's a shortcut for the most common
+	//		Dijit creation modules, _WidgetBase, and _TemplatedMixin.
+	//		Widget also mixes in some useful methods, like show and hide.
+	//		
 	return declare('dx-alias.Widget', [_WidgetBase, _TemplatedMixin], {
 		templateString:'<div></div>', //to be overwritten
 
@@ -36,6 +40,9 @@ define([
 			this._connections.push(h);
 			return h;
 		},
+
+		// add sub() ?
+
 		destroy: function(){
 			this._connections.forEach(function(h){
 				h.remove();
