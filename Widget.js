87975249373ba4523c9f4788ae62e46838730521
Mike Wilcox <mike.wilcox@bettervideo.com>
2012-07-19 15:41:19 -0500

    Add getParent

--- Widget.js
+++ Widget.js
@@ -22,6 +22,44 @@ define([
 			dom.hide(this.domNode);
 		},
 
+		getParent: function(){
+			// summary:
+			//		Returns the parent widget of this widget
+			//console.log('getParent', this.domNode)
+			if(!this.domNode.parentNode){
+				console.warn('NO PARENT', this)
+				return null;
+			}
+			return this._parent || registry.getEnclosingWidget(this.domNode.parentNode) || registry.getEnclosingWidget(this.domNode.parentNode.parentNode);
+		},
+
+		addChildren: function(widgets, node){
+			//console.log('addChildren', widgets)
+			widgets.forEach(function(w){
+				//console.log('addChild', w);
+				this.addChild(w, node);
+			}, this)
+		},
+
+		addChild: function(widget, node){
+			//console.log('addChild')
+			widget._parent = this;
+			node = node || this.containerNode || this.domNode;
+			node.appendChild(widget.domNode);
+			if(this._started && !widget._started) widget.startup();
+			return widget;
+		},
+
+		removeChild: function(widget){
+			if(widget){
+				var node = widget.domNode;
+				if(node && node.parentNode){
+					node.parentNode.removeChild(node); // detach but don't destroy
+				}
+			}
+			return widget;
+		},
+
 		connectEvents: function(){
 			this._connections.forEach(function(handle){ handle.resume(); }, this);
 		},
@@ -31,7 +69,7 @@ define([
 		},
 
 		getObject: function(obj){
-			return typeof obj == 'string' ? registry.byId(obj) : obj;
+			return typeof obj == 'string' ? typeof this[obj] == 'object' ? this[obj] : registry.byId(obj) : obj;
 		},
 
 		on: function(obj, event, ctx, method){
